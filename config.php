0b68bcdeb2231e56135ef4a3886a7948cc4d32f8
hieucode2x <hieutrungvip2002@gmail.com>
2025-04-23 10:33:39 +0700

    fist upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+    'base' => '/htdocs/demo/DEMO/',
+    'baseURL' => 'http://localhost/demo/DEMO/',
+    'assets' => '/demo/DEMO/assets/',
+    'db' => array(
+        'host'     => 'localhost',
+        'name'     => 'productdb',
+        'username' => 'root',
+        'password' => ''
+    )
+);
